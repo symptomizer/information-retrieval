a421e54c06715c6ea6fbf9cb67de00b1c30bc83e
Liana <lianafaahmed@gmail.com>
2021-01-25 09:32:39 +0000

    all done now - I think

--- NHS/A-Z/az-code.py
+++ NHS/A-Z/az-code.py
@@ -1,49 +1,133 @@
 import requests
-import pandas as pd
 import json
 import time
 import string
-from pprint import pprint
+from datetime import date
 
 with open('../nhskey.txt', 'r') as file:
    key = file.read()
 
-url = f'https://api.nhs.uk/conditions/'
-payload = {'subscription-key':key, 'category':'a', 'synonyms':'true'}
+with open('lastChecked.txt', 'r') as file:
+   lastChecked = file.read()
 
-def main():
+main_url = f'https://api.nhs.uk/conditions/'
+main_payload = {'subscription-key':key, 'synonyms':'true'}
 
-    call_counter = 1
+call_counter = 1
+
+def initialise(call_counter, key):
     
-    response = requests.get(url, params=payload)
-    call_counter += 1
-    json_data = response.json()
+    # list of all entries 
+    all_entries = {}
 
-    main_data = pd.json_normalize(json_data['significantLink'])
+    for letter in ['v', 'w', 'x', 'y', 'z']:
 
-    for letter in string.ascii_lowercase:
+        checkCalls(call_counter)
+       
+        print(f'Current Category: {letter}')
 
-        if letter == 'a':
-            continue
-
-        print(call_counter)
-
-        if (call_counter % 10 == 0):
-            time.sleep(60)
-
-        payload['category'] = letter
-
-        response = requests.get(url, params=payload)
+        main_payload['category'] = letter
+        main_response = requests.get(main_url, params=main_payload)
         call_counter += 1
 
-        with open("test.txt", "w") as text_file:
-            text_file.write(response.text)
+        current_cat_data = json.loads(main_response.text)
+        current_payload = {'subscription-key':key}
+        entry_count = 1
+        for entry in current_cat_data['significantLink']:
+
+            print(f'Category {letter} Entry Number: {entry_count}')
+
+            checkCalls(call_counter)
+
+            current_url = entry['url']
+            current_response = requests.get(current_url, params=current_payload)
+            current_json = json.loads(current_response.text)
+            
+            call_counter += 1
+
+            all_entries[entry['name']] = current_response.text
+
+            entry_count += 1
+
+        print(f'Category {letter} is complete.')
     
-        json_data = response.json()
-        current_data = pd.json_normalize(json_data['significantLink'])
+    with open('v-z.json', 'w') as json_file:
+        json.dump(all_entries, json_file)
 
-        main_data = main_data.append(current_data, ignore_index = True)
+    updateLastChecked()
 
-    main_data.to_csv('nhs_az.csv')
+# Because NHS is dumb
+def mergeFiles():
 
-main()
+    merged_json = {}
+
+    with open('a.json', 'r') as a_file:
+        merged_json.update(json.load(a_file))
+
+    with open('b-d.json', 'r') as bd_file:
+        merged_json.update(json.load(bd_file))
+
+    with open('e-h.json', 'r') as eh_file:
+        merged_json.update(json.load(eh_file))
+
+    with open('i-u.json', 'r') as il_file:
+        merged_json.update(json.load(il_file))
+
+    with open('v-z.json', 'r') as vz_file:
+        merged_json.update(json.load(vz_file))
+    
+    # Output merged file
+    with open('nhs_az.json', 'w') as json_file:
+        json.dump(merged_json, json_file)
+
+# Check we're not about to hit the max calls threshold
+def checkCalls(call_counter):
+    if (call_counter % 10 == 0):
+        print("Sleep timer starting")
+        time.sleep(60)
+        print("Sleep timer complete")
+
+# Update last checked date to keep track of when we last updated our dataset
+def updateLastChecked():
+
+    today = date.today()
+    lastChecked = today.strftime("%d-%m-%Y")
+    
+    with open('lastChecked.txt', 'w') as file:
+        file.write(lastChecked)
+
+# Update our dataset with any new entries
+def update(call_counter):
+
+    with open('nhs_az.json', 'r') as json_file:
+        all_entries = json.loads(all_entries, json_file)
+    
+    print(f'Last updated {lastChecked}')
+    update_payload = {'subscription-key':key, 'startDate':lastChecked, 'orderBy':'dateModified'}
+    update_response = requests.get(main_url, params=update_payload)
+    call_counter += 1
+
+    update_data = json.loads(update_response.text)
+
+    for entry in update_data['significantLink']:
+        print(f'Updating entry number: {call_counter}')
+        checkCalls(call_counter)
+
+        current_url = entry['url']
+        current_payload = {'subscription-key':key}
+        current_response = requests.get(current_url, params=current_payload)
+        current_json = json.loads(current_response.text)
+
+        call_counter += 1
+
+        all_entries[entry['name']] = current_response.text
+        print('Updated successfully')
+
+    updateLastChecked()
+
+    with open('nhs_az.json', 'w') as json_file:
+        json.dump(all_entries, json_file)
+    
+    print ('Update successful')
+
+mergeFiles()
