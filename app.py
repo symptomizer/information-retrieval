3ed0d3c1a8688119c5852e8dc225f4ab735c3a11
Ragnor Comerford <hello@ragnor.co>
2021-03-08 21:07:10 +0400

    Add Bert QA model.

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ text = documents['description']+documents['title']+documents['content'].apply(la
 tfidf_model = build_tfidf_model(documents['description']+documents['title'])
 tfidf_faiss = load_faiss(tfidf_model, text, "tfidf")
 bert_faiss = load_faiss(bert_model, text, "bert")
-
+qa_model = QA('models')
 
 @strawberry.type
 class User:
@@ -27,6 +27,11 @@ class SearchResult:
     documents: List[Document]
 
 @strawberry.type
+class QAResult:
+    answer: str
+    confidence : float
+
+@strawberry.type
 class Query:
     @strawberry.field
     def search(self, q: str) -> SearchResult:
@@ -37,5 +42,14 @@ class Query:
         D, I = vector_search(q, bert_model, bert_faiss)
         return SearchResult([Document(id=doc['_id'], title=doc['title'],description=doc['description']) for _, doc in id2details(documents, I).iterrows()])
 
+    @strawberry.field
+    def qa(self, q: str) -> QAResult:
+        D, I = vector_search(q, tfidf_model, tfidf_faiss)
+        reference = ""
+        for _, doc in id2details(documents, I).iterrows():
+            reference += " " + doc["description"]
+        answer = qa_model.predict(reference,q)
+        print(answer['document'])
+        return QAResult(answer = answer['answer'], confidence = answer['confidence'], )
 
 schema = strawberry.Schema(query=Query)
--- build.py
+++ build.py
@@ -1,3 +1,4 @@
+from __future__ import absolute_import, division, print_function
 import nltk
 from collections import defaultdict
 from nltk.stem.snowball import EnglishStemmer  # Assuming we're working with English
@@ -12,12 +13,30 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from cache_to_disk import cache_to_disk
 from cache_to_disk import delete_disk_caches_for_function
 from sentence_transformers import SentenceTransformer
+
+import collections
+import logging
+import math
+
+import numpy as np
+import torch
+from pytorch_transformers import (WEIGHTS_NAME, BertConfig,
+                                  BertForQuestionAnswering, BertTokenizer)
+from torch.utils.data import DataLoader, SequentialSampler, TensorDataset
+
+from utils import (get_answer, input_to_squad_example,
+                   squad_examples_to_features, to_list)
+
+RawResult = collections.namedtuple("RawResult",
+                                   ["unique_id", "start_logits", "end_logits"])
+
 from os import path
 # delete_disk_caches_for_function('get_data')
 
 
 @cache_to_disk(2)
 def get_data():
+    print("Loading documents from database ...")
     client = pymongo.MongoClient('mongodb+srv://ir:UE5Ki3Cr1eyWVeNl@cluster1.xo9vl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
     # get the database
     database = client['document']
@@ -32,7 +51,7 @@ def build_tfidf_model(documents):
 
 def build_bert_model(documents=None):
     print("Building BERT model...")
-    model = SentenceTransformer('paraphrase-distilroberta-base-v1')
+    model = SentenceTransformer('msmarco-distilbert-base-v2')
     print("Completed BERT model.")
     return model
 
@@ -52,16 +71,73 @@ def build_faiss(model, documents, name):
     # index = faiss.IndexIDMap(index)
     # Step 4: Add vectors and their IDs
     index.add(embeddings)
-    faiss.write_index(index,f"{name}.index")
+    faiss.write_index(index,f"models/{name}.index")
     print(f"Completed {name} index.")
     return index
     
 
 def load_faiss(model, documents, name):
-    if path.exists(f"{name}.index"):
-        return faiss.read_index(f"{name}.index")
+    if path.exists(f"models/{name}.index"):
+        return faiss.read_index(f"models/{name}.index")
     else:
         return build_faiss(model, documents, name)
+        
+
+class QA:
+
+    def __init__(self,model_path: str):
+        self.max_seq_length = 384
+        self.doc_stride = 128
+        self.do_lower_case = True
+        self.max_query_length = 64
+        self.n_best_size = 20
+        self.max_answer_length = 30
+        self.model, self.tokenizer = self.load_model(model_path)
+        if torch.cuda.is_available():
+            self.device = 'cuda'
+        else:
+            self.device = 'cpu'
+        self.model.to(self.device)
+        self.model.eval()
+
+
+    def load_model(self,model_path: str,do_lower_case=False):
+        config = BertConfig.from_pretrained(model_path + "/bert_config.json")
+        tokenizer = BertTokenizer.from_pretrained(model_path, do_lower_case=do_lower_case)
+        model = BertForQuestionAnswering.from_pretrained(model_path, from_tf=False, config=config)
+        return model, tokenizer
+    
+    def predict(self,passage :str,question :str):
+        example = input_to_squad_example(passage,question)
+        features = squad_examples_to_features(example,self.tokenizer,self.max_seq_length,self.doc_stride,self.max_query_length)
+        all_input_ids = torch.tensor([f.input_ids for f in features], dtype=torch.long)
+        all_input_mask = torch.tensor([f.input_mask for f in features], dtype=torch.long)
+        all_segment_ids = torch.tensor([f.segment_ids for f in features], dtype=torch.long)
+        all_example_index = torch.arange(all_input_ids.size(0), dtype=torch.long)
+        dataset = TensorDataset(all_input_ids, all_input_mask, all_segment_ids,
+                                all_example_index)
+        eval_sampler = SequentialSampler(dataset)
+        eval_dataloader = DataLoader(dataset, sampler=eval_sampler, batch_size=1)
+        all_results = []
+        for batch in eval_dataloader:
+            batch = tuple(t.to(self.device) for t in batch)
+            with torch.no_grad():
+                inputs = {'input_ids':      batch[0],
+                        'attention_mask': batch[1],
+                        'token_type_ids': batch[2]  
+                        }
+                example_indices = batch[3]
+                outputs = self.model(**inputs)
+
+            for i, example_index in enumerate(example_indices):
+                eval_feature = features[example_index.item()]
+                unique_id = int(eval_feature.unique_id)
+                result = RawResult(unique_id    = unique_id,
+                                    start_logits = to_list(outputs[0][i]),
+                                    end_logits   = to_list(outputs[1][i]))
+                all_results.append(result)
+        answer = get_answer(example,features,all_results,self.n_best_size,self.max_answer_length,self.do_lower_case)
+        return answer
 
  
 class Index:
